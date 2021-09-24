8f9292b62918d83d7bd6b6c067b77d2427f2f8c0
stragesnight <alex.spresyo.o@gmail.com>
2021-09-24 14:42:00 +0300

    stuff

---
+++ DZ_02/Task01.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * 	-= Домашная Работа №2, Задание №1 =-
+ * 		Ученик: Шелест Александр
+ *
+ * 	Вывести все положительные числа от A до B (A < B).
+ * 	Каждое число должно выводится с новой строки, при этом 
+ * 	количество повторений числа равно его значению.
+ */
+
+using System;
+
+class Task01
+{
+	static void Main(string[] args)
+	{
+		const uint d_min = 1;
+		const uint d_max = 16;
+
+		uint a;
+		uint b;
+		
+		Console.WriteLine("-= Домашная Работа №2, Задание №1 =-");
+		Console.WriteLine("\tУченик: Шелест Александр\n");
+		Console.WriteLine("Вывести все положительные числа от A до B (A < B).");
+		Console.WriteLine("Каждое число должно выводится с новой строки, при этом");
+		Console.WriteLine("количество повторений числа равно его значению.\n");
+
+		// ввод пользователем диапазона с проверкой на корректность
+		do
+		{
+			Console.Write("Введите нижнюю границу диапазона A [{0} .. {1}]: ",
+					d_min, d_max - 1);
+
+		} while (!uint.TryParse(Console.ReadLine(), out a) 
+				|| !InRange(a, d_min, d_max - 1));
+
+		do
+		{
+			Console.Write("Введите верхнюю границу диапазона B [{0} .. {1}]: ",
+					a, d_max);
+
+		} while (!uint.TryParse(Console.ReadLine(), out b) 
+				|| !InRange(b, a, d_max));
+
+		Console.WriteLine("\nРезультирующий диапазон: [{0} .. {1}]", a, b);
+
+		// вывод последовательности чисел
+		for (uint i = a; i <= b; i++)
+		{
+			for (uint j = 0; j < i; j++)
+				Console.Write("{0} ", i);
+			Console.Write('\n');
+		}
+
+		Console.WriteLine("\nНажмите любую клавишу для выхода из программы...");
+		Console.ReadKey();
+	}
+
+	// Проверить, входит ли число в диапазон
+	private static bool InRange(uint v, uint min, uint max)
+		=> (v >= min) && (v <= max);
+}
+
---
+++ DZ_02/Task02.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * 	-= Домашная Работа №2, Задание №2 =-
+ * 		Ученик: Шелест Александр
+ *
+ * 	Проверить, является ли введённый пользователем билет
+ * 	"счастливым", т.е. проверить, совпадает ли сумма цифр
+ * 	левой части с суммой цифр правой части.
+ */
+
+using System;
+
+class Task02
+{
+	static void Main(string[] args)
+	{
+		string ticket;
+
+		Console.WriteLine("-= Домашная Работа №2, Задание №2 =-");
+		Console.WriteLine("\tУченик: Шелест Александр\n");
+		Console.WriteLine("Проверить, является ли введённый пользователем билет");
+		Console.WriteLine("\"счастливым\", т.е. проверить, совпадает ли сумма цифр");
+		Console.WriteLine("левой части с суммой цифр правой части.");
+ 
+		// ввод пользователем строки с проверкой на корректность
+		do
+		{
+			Console.Write("Введите номер билета (шестизначное число): ");
+			ticket = Console.ReadLine();
+
+		} while (ticket.Length != 6 || !IsNumericString(ticket));
+
+		Console.WriteLine("\nИдёт проверка билета...\n");
+
+		// выбор строки в зависимости от результата проверки
+		string res = IsLuckyTicket(ticket) 
+					? "билет счастилвый!"
+					: "билет не счастливый...";
+
+		Console.WriteLine("Результат: {0}", res);
+
+		Console.WriteLine("\nНажмите любю клавишу для выхода из программы...\n");
+		Console.ReadKey();
+	}
+
+	// Проверить, соответствует ли строка числовому значению
+	private static bool IsNumericString(string str)
+	{
+		for (int i = 0; i < str.Length; i++)
+		{
+			// если хотя бы один символ не является цифрой,
+			// то строка не подходит по критерию
+			if (str[i] < '0' || str[i] > '9')
+				return false;
+		}
+
+		return true;
+	}
+
+	// Проверить, является ли билет счастливым
+	private static bool IsLuckyTicket(string str)
+	{
+		ushort sum_left = 0;
+		ushort sum_right = 0;
+
+		// так как на, по сути, не важны сами числовые значения строки,
+		// можно считать сумму символьных значений
+		for (ushort i = 0; i < 3; i++)
+			sum_left += str[i];
+		for (ushort i = 3; i < 6; i++)
+			sum_right += str[i];
+
+		return sum_left == sum_right;
+	}
+}
+
